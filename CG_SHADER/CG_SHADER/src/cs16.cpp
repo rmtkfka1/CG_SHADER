206915dd5d3b3b1a1cba2ee2569e1b4c5f80674b
rmtkfka1 <132369391+rmtkfka1@users.noreply.github.com>
2023-10-14 17:26:02 +0900

    도형 으로 바꾸기만 구현하면됨

--- CG_SHADER/CG_SHADER/src/cs16.cpp
+++ CG_SHADER/CG_SHADER/src/cs16.cpp
@@ -8,12 +8,13 @@ void cs16::Init()
 
 	cube.Init(cube_m_pos, 0.1f, 0);
 	line.Init();
-	mode = e_revolve;
-	cube_revolve.y = 0.0f;
-	cube_rotate.x = 10.0f;
-
+	mode = revolve;
+	cb.revolve.y = 0.0f;
+	cb.rotate.x = 10.0f;
 	////////////////////////
-	pr.Init(pr_m_pos, 0.1f, 0);
+	pyramid.Init(pr_m_pos, 0.1f, 0);
+	pr.revolve.y = 0.0f;
+	pr.rotate.x = 10.0f;
 
 }
 
@@ -35,31 +36,55 @@ void cs16::Render()
 	//===================================================================================//
 	
 
-	if (mode == e_revolve)
+	if (mode == revolve) //둘다 공전
 	{
-		t1 = glm::rotate(glm::mat4(1.0f), glm::radians(cube_revolve.y), glm::vec3(0, 1.0f, 0));
-		t2 = glm::rotate(glm::mat4(1.0f), glm::radians(10.0f), glm::vec3(1.0f, 0, 0));
-		result = t2 * t1;
+		cb.t1 = glm::rotate(glm::mat4(1.0f), glm::radians(cb.revolve.y), glm::vec3(0, 1.0f, 0));
+		cb.t2 = glm::rotate(glm::mat4(1.0f), glm::radians(10.0f), glm::vec3(1.0f, 0, 0));
+		cb.result = cb.t2 * cb.t1;
 
-		v[0]->SetUniformMat4f("u_model", result);
-		temp = result * glm::vec4(cube_m_pos.x, cube_m_pos.y, cube_m_pos.z, 1);
+		v[0]->SetUniformMat4f("u_model", cb.result);
+		cb.temp = cb.result * glm::vec4(cube_m_pos.x, cube_m_pos.y, cube_m_pos.z, 1);
+		cube.Render();
+
+
+		pr.t1 = glm::rotate(glm::mat4(1.0f), glm::radians(pr.revolve.y), glm::vec3(0, 1.0f, 0));
+		pr.t2 = glm::rotate(glm::mat4(1.0f), glm::radians(10.0f), glm::vec3(1.0f, 0, 0));
+		pr.result = pr.t2 * pr.t1;
+
+		v[0]->SetUniformMat4f("u_model", pr.result);
+		pr.temp = pr.result * glm::vec4(pr_m_pos.x, pr_m_pos.y, pr_m_pos.z, 1);
+		pyramid.Render();
+
 	}
 
-	else if (mode == e_rotate)
-	{
-		auto goto00 = glm::translate(glm::mat4(1.0f), glm::vec3(-temp.x, -temp.y, -temp.z));
-		t3 = glm::rotate(glm::mat4(1.0f), glm::radians(cube_rotate.y), glm::vec3(0, 1.0f, 0));
-		auto t4 = glm::rotate(glm::mat4(1.0f), glm::radians(cube_rotate.x), glm::vec3(1.0f, 0, 0));
-		auto backtopos = glm::translate(glm::mat4(1.0f), glm::vec3(temp.x, temp.y, temp.z));
 
-		auto result_v = backtopos * t4*t3 * goto00 * result;
-		v[0]->SetUniformMat4f("u_model", result_v);
+
+	else if (mode == Mode::rotate) // 둘다 자전
+	{
+		cb.goto00 = glm::translate(glm::mat4(1.0f), glm::vec3(-cb.temp.x, -cb.temp.y, -cb.temp.z));
+		cb.t3 = glm::rotate(glm::mat4(1.0f), glm::radians(cb.rotate.y), glm::vec3(0, 1.0f, 0));
+		cb.t4 = glm::rotate(glm::mat4(1.0f), glm::radians(cb.rotate.x), glm::vec3(1.0f, 0, 0));
+		cb.backtopos = glm::translate(glm::mat4(1.0f), glm::vec3(cb.temp.x, cb.temp.y, cb.temp.z));
+
+		cb.result2 = cb.backtopos * cb.t4*cb.t3 * cb.goto00 * cb.result;
+		v[0]->SetUniformMat4f("u_model", cb.result2);
+		cube.Render();
+
+		pr.goto00 = glm::translate(glm::mat4(1.0f), glm::vec3(-pr.temp.x, -pr.temp.y, -pr.temp.z));
+		pr.t3 = glm::rotate(glm::mat4(1.0f), glm::radians(pr.rotate.y), glm::vec3(0, 1.0f, 0));
+		pr.t4 = glm::rotate(glm::mat4(1.0f), glm::radians(pr.rotate.x), glm::vec3(1.0f, 0, 0));
+		pr.backtopos = glm::translate(glm::mat4(1.0f), glm::vec3(pr.temp.x, pr.temp.y, pr.temp.z));
+
+		pr.result2 = pr.backtopos * pr.t4 * pr.t3 * pr.goto00 * pr.result;
+		v[0]->SetUniformMat4f("u_model", pr.result2);
+		pyramid.Render();
+
 	}
 
+	
+	
+	
 
-
-	pr.Render();
-	cube.Render();
 
 
 }
@@ -73,28 +98,155 @@ void cs16::Update_key(unsigned char key, int x, int y)
 {
 	
 	auto& v = ObjectManager::GetInstance()->Get_Shader();
-	if (key == 'r') //공전함
+	if (key == 'r') //둘다 양의 뱡향으로 공전함
 	{
-		mode = e_revolve;
-		cube_revolve.y += 10.0f;
+		mode = revolve;
+		cb.revolve.y += 10.0f;
+		pr.revolve.y += 10.0f;
 	}
 
-	if (key == 'R') //공전함
+	if (key == 'R') //둘다 음의 방향으로 공전함
 	{
-		mode = e_revolve;
-		cube_revolve.y -= 10.0f;
+		mode = revolve;
+		cb.revolve.y -= 10.0f;
+		pr.revolve.y -= 10.0f;
 	}
 
-	if (key == '1') //자기자리에서 자전함
+	if (key == 'x')
 	{
-		mode = e_rotate;
-		cube_rotate.y += 10.0f;
+		mode2 = rotate_x_plus;
 	}
-	if (key == '2') //자기자리에서 자전함
+
+	if (key == 'X')
 	{
-		mode = e_rotate;
-		cube_rotate.y -= 10.0f;
+		mode2 = rotate_x_minus;
 	}
+
+	if (key == 'y')
+	{
+		mode2 = rotate_y_plus;
+	}
+
+	if (key == 'Y')
+	{
+		mode2 = rotate_y_minus;
+	}
+
+
+	if (key == '1') //좌측의 객체만 mode2 에 값에 따라 자전함
+	{
+	
+		if (mode2 == rotate_x_plus)
+		{
+			mode = Mode::rotate;
+			cb.rotate.x += 10.0f;
+		}
+		if (mode2 == rotate_x_minus)
+		{
+			mode = Mode::rotate;
+			cb.rotate.x -= 10.0f;
+		}
+		if (mode2 == rotate_y_plus)
+		{
+			mode = Mode::rotate;
+			cb.rotate.y += 10.0f;
+		}
+		if (mode2 == rotate_y_minus)
+		{
+			mode = Mode::rotate;
+			cb.rotate.y -= 10.0f;
+		}
+
+	}
+	if (key == '2') //우측의 객체만 음의 방향으로 자전
+	{
+		if (mode2 == rotate_x_plus)
+		{
+			mode = Mode::rotate;
+			pr.rotate.x += 10.0f;
+		}
+		if (mode2 == rotate_x_minus)
+		{
+			mode = Mode::rotate;
+			pr.rotate.x -= 10.0f;
+		}
+		if (mode2 == rotate_y_plus)
+		{
+			mode = Mode::rotate;
+			pr.rotate.y += 10.0f;
+		}
+		if (mode2 == rotate_y_minus)
+		{
+			mode = Mode::rotate;
+			pr.rotate.y -= 10.0f;
+		}
+	}
+
+	if (key == '3') //양쪽 객체모두 자전
+	{
+
+		if (mode2 == rotate_x_plus)
+		{
+			mode = Mode::rotate;
+			cb.rotate.x += 10.0f;
+		}
+		if (mode2 == rotate_x_minus)
+		{
+			mode = Mode::rotate;
+			cb.rotate.x -= 10.0f;
+		}
+		if (mode2 == rotate_y_plus)
+		{
+			mode = Mode::rotate;
+			cb.rotate.y += 10.0f;
+		}
+		if (mode2 == rotate_y_minus)
+		{
+			mode = Mode::rotate;
+			cb.rotate.y -= 10.0f;
+		}
+
+		if (mode2 == rotate_x_plus)
+		{
+			mode = Mode::rotate;
+			pr.rotate.x += 10.0f;
+		}
+		if (mode2 == rotate_x_minus)
+		{
+			mode = Mode::rotate;
+			pr.rotate.x -= 10.0f;
+		}
+		if (mode2 == rotate_y_plus)
+		{
+			mode = Mode::rotate;
+			pr.rotate.y += 10.0f;
+		}
+		if (mode2 == rotate_y_minus)
+		{
+			mode = Mode::rotate;
+			pr.rotate.y -= 10.0f;
+		}
+	}
+
+	if (key == 's')
+	{
+		mode = revolve;
+		cb.revolve.y = 0.0f;
+		cb.rotate.x = 10.0f;
+		pr.revolve.y = 0.0f;
+		pr.rotate.x = 10.0f;
+
+		cb.revolve.y = 0.0f;
+		cb.rotate.x = 10.0f;
+		cb.rotate.y = 0;
+		pr.revolve.y = 0.0f;
+		pr.rotate.x = 10.0f;
+		pr.rotate.y = 0;
+
+		cube_m_pos={ -0.9f ,0.3f, 0 }; 
+		pr_m_pos = { 0.9f,0.3f,0 };
+	}
+
 
 	glutPostRedisplay();
 }
--- CG_SHADER/CG_SHADER/src/cs16.h
+++ CG_SHADER/CG_SHADER/src/cs16.h
@@ -4,9 +4,31 @@
 #include "Pyramid.h"
 #include "Line.h"
 enum Mode {
-	e_rotate,
-	e_revolve,
+	revolve,
+	rotate,
+};
 
+enum XY {
+	rotate_x_plus,
+	rotate_x_minus,
+	rotate_y_plus,
+	rotate_y_minus,
+};
+
+struct my
+{
+
+	glm::vec4 temp;
+	glm::mat4 t1;
+	glm::mat4 t2;
+	glm::mat4 t3;
+	glm::mat4 t4;
+	glm::mat4 goto00;
+	glm::mat4 backtopos;
+	glm::mat4 result;
+	glm::mat4 result2;
+	vec2 revolve; //공전
+	vec2 rotate; //자전
 
 };
 
@@ -34,25 +56,20 @@ private:
 
 	Line line;
 	Mode mode;
-
-
+	XY mode2;
 
 	////////////////////////큐브///////////////////////////////
 	Cube cube;
 	vec3 cube_m_pos = { -0.9f ,0.3f, 0 };//초기 포지션 
-
-	glm::vec4 temp;
-	glm::mat4 t1;
-	glm::mat4 t2;
-	glm::mat4 t3;
-	glm::mat4 result;
-	vec2 cube_revolve; //공전
-	vec2 cube_rotate; //자전
-
 	////////////////////////삼각뿔///////////////////////////////
-	Pyramid pr;
-	vec3 pr_m_pos = { 0.9f ,0.3f, 0 };//초기 포지션 
+	Pyramid pyramid;
+	vec3 pr_m_pos = { 0.9f,0.3f,0 };
 
+	my cb;
+	my pr;
+	
+
+	
 
 };
 
